ddf8adf6f358e5af921fc615b4817b1f4d3345c0
Ryder <dev@ryderdamen.com>
2018-10-23 23:26:43 -0400

    updating gitignore, npmignore, and adding jest

---
+++ tests/knope.test.js
@@ -0,0 +1,30 @@
+const knope = require('../knope');
+
+test('Gets a simple compliment', () => {
+    expect(knope.getCompliment('Ann')).toEqual(expect.anything())
+})
+
+
+// // Test one compliment
+// var compliment = knope.getCompliment('Ann')
+// console.log(compliment)
+
+// // Test two compliments
+// var compliment = knope.getCompliment('Ann', 2)
+// console.log(compliment)
+
+// // Test three compliments without an oxford comma
+// var compliment = knope.getCompliment('Ann', 3, false)
+// console.log(compliment)
+
+// // Test a random number of compliments
+// var compliment = knope.getCompliment('Ann', 'random', false)
+// console.log(compliment)
+
+// // Test a compliment with alliteration
+// var compliment = knope.getComplimentWithAlliteration('Ann')
+// console.log(compliment)
+
+// // Test an official compliment
+// var compliment = knope.getOfficialCompliment('Ann')
+// console.log(compliment)
